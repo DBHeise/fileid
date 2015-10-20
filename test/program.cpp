05489d9884b5ee23c219f27ff6f62e022b055933
CrazyDave <DBHeise@users.noreply.github.com>
2015-10-20 05:50:47 -0400

    Adding stub for future test app

---
+++ test/program.cpp
@@ -0,0 +1,18 @@
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+
+
+int main(int argc, char* argv[])
+{
+	std::vector<std::string> params(argv + 1, argv + argc);
+	std::string fit = params[0];
+	
+
+
+	return 0;
+}
