901591919c8b53fac66f7e7c965c2da40364782a
CrazyDave <DBHeise@users.noreply.github.com>
2015-11-12 20:45:02 -0500

    Simple test script

---
+++ test/simpletest.ps1
@@ -0,0 +1,13 @@
+﻿$testFolder = 'C:\TestFiles\Sample'
+Set-Alias fileid (Resolve-Path ..\x64\debug\fileid.exe)
+
+gci $testFolder | ?{!$_.PSIsContainer} | % {
+    $r = fileid $_.FullName text
+    $rExt = $r.Split()[2].Trim()
+    if ($rExt -ne $_.Extension.Trim('.')) {        
+        New-Object PSObject -Property @{
+            File = $_.FullName
+            Result = $r
+        }
+    } 
+}
