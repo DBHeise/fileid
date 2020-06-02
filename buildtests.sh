3b6abf03738cb45597d5ab80981771761a7602b7
DBHeise <david@heiseink.com>
2020-06-02 02:50:46 -0400

    bugfix in linux test-build script

--- buildtests.sh
+++ buildtests.sh
@@ -2,6 +2,6 @@ cd ./test
 
 g++ -std=c++14 -O3 common_test.cpp -o ../bin/common_test
 g++ -std=c++14 -O3 oless/olecommon_test.cpp -o ../bin/olecommon_test
-g++ -std=c++14 -O3 oless/structures/LongRGB_test.cpp -o ../bin/LongRGB_test
+g++ -std=c++14 -O3 oless/excel/structures/LongRGB_test.cpp -o ../bin/LongRGB_test
 
 cd ..
